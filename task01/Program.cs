0e3ff7b25320637ce93661b40d21321807f46732
Barbara_Butova <butova95@ya.ru>
2022-11-27 20:44:22 +0300

    создано решение задачи

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("start");
+int FirstNumber = new Random ().Next(1,1000);
+int SecondNumber = new Random ().Next(1,1000);
+Console.WriteLine(FirstNumber);
+Console.WriteLine(SecondNumber);
+int SquareSecondNumber = SecondNumber * SecondNumber;
+if (SquareSecondNumber == FirstNumber);
+do Console.WriteLine("Первое число является квадратом второго")
+else Console.WriteLine("Первое число НЕ является квадратом второго")
+Console.WriteLine("end")
+
