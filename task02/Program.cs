302ab2d16f92c7664a8c69619a3ed38ad354a340
Barbara_Butova <butova95@ya.ru>
2022-12-01 16:34:07 +0300

    задача решена

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+double number01 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double number02 = Convert.ToDouble(Console.ReadLine());
+if (number01>number02)
+{
+    Console.WriteLine("Большее число " + number01);
+    Console.WriteLine("Меньшее число " + number02);
+}
+else
+{
+    Console.WriteLine("Большее число " + number02);
+    Console.WriteLine("Меньшее число " + number01);
+}    
